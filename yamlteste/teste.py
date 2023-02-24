dc59a7ddc7cd0171be5c93cb52944e073d7b9192
alceudeggerone <alceu.deggerone@gmail.com>
2023-02-24 20:40:53 -0300

    b

--- yamlteste/teste.py
+++ yamlteste/teste.py
@@ -1,11 +1,12 @@
 import requests
 import yaml
 import json
-url = 'https://raw.githubusercontent.com/alceudeggerone/glue/master/yaml/teste.yaml'
+url = 'https://raw.githubusercontent.com/alceudeggerone/glue/master/yamlteste/teste.yaml'
 payload={}
 
 response = requests.request("GET", url, verify=False)
 
 a=response.content
 data=yaml.safe_load(a)
+
 print(data)
