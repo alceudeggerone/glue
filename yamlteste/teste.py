653e196b1dde2b39780a5d7b779b01c9b2a18e4d
alceudeggerone <alceu.deggerone@gmail.com>
2023-02-24 20:52:02 -0300

    b

--- yamlteste/teste.py
+++ yamlteste/teste.py
@@ -2,6 +2,7 @@ import requests
 import yaml
 import json
 url = 'https://raw.githubusercontent.com/alceudeggerone/glue/master/yamlteste/teste.yaml'
+
 payload={}
 
 response = requests.request("GET", url, verify=False)
@@ -10,5 +11,7 @@ a=response.content
 data=yaml.safe_load(a)
 
 print(data)
+
+
 
 
